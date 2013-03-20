2be66b0f15e57df427159d641aacdd441c91eeaf
Ken Lu <ken@seeice.com>
2013-03-20 09:32:17 -0400

    Added gmap and show the basic projec location info at map

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gem 'rails', '3.2.13.rc1'
+gem 'gmaps4rails'
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
   gem 'coffee-rails', '~> 3.2.1'
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,12 @@
 class Project
   include Mongoid::Document
   include Mongoid::Timestamps
-  has_one :location
+  #has_one :location
+  include Gmaps4rails::ActsAsGmappable
+  #include Mongoid::Geo
   field :name, type: String
   field :created_at, type: Time
   field :description, type: String
+  acts_as_gmappable :position => :location, :process_geocoding => false
+  field :location, :type => Array
 end
