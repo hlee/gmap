613eb9676ae7e21d56d76c8d40a8da19cdb212c3
Ken Lu <ken@seeice.com>
2013-03-25 07:45:49 -0400

    function in model get location information and convert to latitude and lngtitude

--- app/models/project.rb
+++ app/models/project.rb
@@ -12,4 +12,17 @@ class Project
   def to_latlng
     self.location.first.to_s + ", " + self.location.last.to_s
   end
+  
+  def round_to_latlng
+    self.location.first.to_f.round(4).to_s + self.location.last.to_f.round(4).to_s
+  end
+
+  def self.fetch_location options
+    latlng = options[:lat].to_f.round(4).to_s + options[:lng].to_f.round(4).to_s
+    Project.all.select{|x|x.round_to_latlng == latlng}.first.dis_info
+  end
+  
+  def dis_info
+    "Name: " + self.name + " \nDescription:" + self.description 
+  end
 end
