821229382ba92d6e92c04208372f757fb6eccd1a
Ken Lu <ken@seeice.com>
2013-04-11 08:39:33 -0400

    Fixed the params bug

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -4,10 +4,15 @@ module ProjectsHelper
     project.name
   end
  
-  def parameter_options
+  def parameter_options id
     options = %w(salinity temperature oxygen saturation)
     arr = []
     options.each_with_index{|x,i|arr << [x, i]}
-    options_for_select arr
+    options_for_select(arr, [options[id.to_i], id])
   end
+ 
+  def d_name id
+    Department.where(id: id).first.name
+  end
+
 end
